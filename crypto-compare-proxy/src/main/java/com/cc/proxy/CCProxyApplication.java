0866f5290e5425849a7d381af2641af683eb7c38
T.N.Silverman <tnsilver@gmail.com>
2022-08-30 15:51:47 +0300

    renamed main class

--- crypto-compare-proxy/src/main/java/com/cc/proxy/CCProxyApplication.java
+++ crypto-compare-proxy/src/main/java/com/cc/proxy/CCProxyApplication.java
@@ -6,14 +6,14 @@ import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @PropertySource({ "classpath:/application.properties", "classpath:/rest.properties", "classpath:/cache.properties", "classpath:admin-key.properties" })
-public class CryptoCompareProxyApplication {
+public class CCProxyApplication {
 
 	/**
 	 * The main entry point into this spring boot application
 	 * @param args the array of string arguments
 	 */
 	public static void main(String[] args) {
-		SpringApplication.run(CryptoCompareProxyApplication.class, args);
+		SpringApplication.run(CCProxyApplication.class, args);
 	}
 
 }
