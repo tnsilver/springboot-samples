a27ec80616591a5f12bd51eb3821659195d68415
T.N.Silverman <tnsilver@gmail.com>
2022-08-31 13:44:27 +0300

    converted to war project - can now be deployed on Tomcat

--- crypto-compare-proxy/src/main/java/com/cc/proxy/CCProxyApplication.java
+++ crypto-compare-proxy/src/main/java/com/cc/proxy/CCProxyApplication.java
@@ -2,11 +2,12 @@ package com.cc.proxy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @PropertySource({ "classpath:/application.properties", "classpath:/rest.properties", "classpath:/cache.properties", "classpath:admin-key.properties" })
-public class CCProxyApplication {
+public class CCProxyApplication extends SpringBootServletInitializer {
 
 	/**
 	 * The main entry point into this spring boot application
