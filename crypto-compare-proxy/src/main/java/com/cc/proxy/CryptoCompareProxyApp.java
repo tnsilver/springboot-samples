8d05931106f17fc2e8d0a31f96e0c57c3a13b5e3
T.N.Silverman <tnsilver@gmail.com>
2022-09-02 14:07:26 +0300

    renamed app entry point class

--- crypto-compare-proxy/src/main/java/com/cc/proxy/CryptoCompareProxyApp.java
+++ crypto-compare-proxy/src/main/java/com/cc/proxy/CryptoCompareProxyApp.java
@@ -7,14 +7,14 @@ import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @PropertySource({ "classpath:/application.properties", "classpath:/rest.properties", "classpath:/cache.properties", "classpath:admin-key.properties" })
-public class CCProxyApplication extends SpringBootServletInitializer {
+public class CryptoCompareProxyApp extends SpringBootServletInitializer {
 
 	/**
 	 * The main entry point into this spring boot application
 	 * @param args the array of string arguments
 	 */
 	public static void main(String[] args) {
-		SpringApplication.run(CCProxyApplication.class, args);
+		SpringApplication.run(CryptoCompareProxyApp.class, args);
 	}
 
 }
